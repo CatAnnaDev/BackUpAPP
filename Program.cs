b3ecbe254054e51ab2010030647840b2cd50bb72
PsykoDev <johann.l_62@hotmail.fr>
2022-06-26 16:51:14 +0200

    update config file from json to DB

--- Program.cs
+++ Program.cs
@@ -10,6 +10,7 @@ namespace BackUpAPP
         {
             // To customize application configuration such as set high DPI settings or default font,
             // see https://aka.ms/applicationconfiguration.
+            
             ApplicationConfiguration.Initialize();
             Application.Run(new Form1());
         }
